2e6315fbe6607c6feb69036ec1a7c107f68efc4f
Richard Bouška <richard.bouska@gmail.com>
2016-09-23 14:47:52 +0200

    still strugling with JEST

--- src/example/chapter1.spec.js
+++ src/example/chapter1.spec.js
@@ -2,7 +2,7 @@
  * Created by Richard Bouška on 22. 9. 2016.
  */
 
-import { str } from '../lib/main';
+import { str } from '../../lib/main';
 
 
 test('str contract accepts a string param', () => { // eslint-disable-line no-undef
