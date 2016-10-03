ffa5ecbebf167ff0d9b93a7ad1d7b11a4b49811d
Richard Bouška <richard@bouska.cz>
2016-10-03 15:22:26 +0200

    ongiong ...

--- src/example/chapter1.spec.js
+++ src/example/chapter1.spec.js
@@ -1,10 +1,10 @@
 /**
  * Created by Richard Bouška on 22. 9. 2016.
  */
-jest.unmock('../lib/main'); // eslint-disable-line no-undef
+
 import { str } from '../lib/main';
 
 
 describe('str contract accepts a string param', () => { // eslint-disable-line no-undef
-  expect(str('some text')).toBe('some tex t'); // eslint-disable-line no-undef
+  expect(str('some text')).toBe('some text'); // eslint-disable-line no-undef
 });
